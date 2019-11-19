5d5ce79246f95860b084f50b06e89f7c07712302
Ri-Hani HENDRIKS <rhendrik@c4r25s1.wethinkcode.co.za>
2019-11-19 13:35:54 +0200

    6:39 busy making DB wrpper

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,11 +1,15 @@
 <?php
 
 class Home extends Controller {
-		public function __construct($controller, $action) {
-				parent::__construct($controller, $action);
-		}
+	public function __construct($controller, $action) {
+		parent::__construct($controller, $action);
+	}
 
-		public function indexAction(){
-				$this->view->render('home/index');
-		}
+	public function indexAction(){
+		$db = DB::getInstance();
+		$sql = "SELECT * FROM test";
+		$testQ = $db->query($sql);
+		dnd($testQ);
+		$this->view->render('home/index');
+	}
 }
