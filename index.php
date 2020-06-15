259dd8a27cbc3bc56c2c8db2e7dc89b153bff983
rhendrik <rhendrik@student.wethinkcode.co.za>
2020-06-15 04:42:20 +0200

    css framework added and header and footer

--- index.php
+++ index.php
@@ -2,14 +2,24 @@
 <html>
 <head>
 	<title>camagru</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 	<!-- header -->
-	<header></header>
+	<header class="section">
+		<div class="hero">
+			<h1 class="hero-title has-text-centered">Camagru</h1>
+		</div>
+	</header>
 
 	<!-- body -->
 
 	<!-- footer -->
-	<footer></footer>
+	<footer>
+		<div class="hero">
+			<h1 class="hero-body has-text-centered">capture something</h1>	
+		</div>
+	</footer>
 </body>
 </html>
